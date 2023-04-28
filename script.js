86410292da99ab436b771f93df982d17d4d12ced
Praveen Katikireddy <praveenk9417@gmail.com>
2023-04-28 16:23:20 +0530

    created a submission

--- script.js
+++ script.js
@@ -13,3 +13,29 @@ const apiUrls = [
 ];
 
 // You can write your code here
+function fetchData(url) {
+	return fetch(url).then((response)=>response.json());
+}
+function timePromise(promise) {
+	const start = performance.now();
+	return promise.then((data)=>{
+		console.log(data);
+		return performance.now()-start
+	});
+}
+function fetchAll() {
+	const start = performance.now();
+	const promises = apiUrls.map(fetchData);
+	return Promise.all(promises).then(()=>performance.now());
+}
+function fetchAny() {
+	const start = performance.now();
+	const promises = apiUrls.map(fetchData);
+	return Promise.any(promises).then(()=>performance.now());
+}
+Promise.all([timePromise(fetchAll()),timePromise(fetchAny())]).then(
+  ([timeAll, timeAny]) => {
+    document.getElementById("output-all").innerText = timeAll;
+    document.getElementById("output-any").innerText = timeAny;
+  }
+);
